c4db936c008b80ec80e5565ff152b0a1fbde7666
kekmekci <k.ekmekci@outlook.de>
2022-11-10 01:22:04 +0100

    var types added

--- start.js
+++ start.js
@@ -1,1 +1,14 @@
-console.log("hello world!")
+let greeting
+let age = 19
+let male = true
+
+greeting = "Hello World!"
+
+console.log(typeof greeting)
+console.log(greeting)
+
+console.log(typeof age)
+console.log(age)
+
+console.log(typeof male)
+console.log(male)
